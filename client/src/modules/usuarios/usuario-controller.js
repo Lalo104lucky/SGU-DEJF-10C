aea9e27b4c29b3e1556b3358ac3c282169453c22
Lalo104lucky <20223tn021@utez.edu.mx>
2025-11-11 21:13:39 -0600

    [ADD] Docker funcionando para ver si jala con Jenkins

---
+++ client/src/modules/usuarios/usuario-controller.js
@@ -0,0 +1,53 @@
+const UsuarioController = {};
+const ENV = import.meta.env;
+
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+
+UsuarioController.getUsuarios = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+};
+
+UsuarioController.getUsuarioById = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`);
+    const data = await response.json();
+    return data;
+};
+
+UsuarioController.createUsuario = async (usuario) => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+    });
+    const data = await response.json();
+    return data;
+};
+
+UsuarioController.updateUsuario = async (usuario) => {
+    const response = await fetch(`${API_URL}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+    });
+    const data = await response.json();
+    return data;
+}
+
+UsuarioController.deleteUsuario = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await response.json();
+    return data;
+};
+
+export default UsuarioController;
