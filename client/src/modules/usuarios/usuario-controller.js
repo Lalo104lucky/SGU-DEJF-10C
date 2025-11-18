b274b10dc6cccbc4d0237f6e97457205936a0d03
Lalo104lucky <20223tn021@utez.edu.mx>
2025-11-17 22:30:05 -0600

    [ADD] Nginx para HTTPS

--- client/src/modules/usuarios/usuario-controller.js
+++ client/src/modules/usuarios/usuario-controller.js
@@ -1,7 +1,7 @@
 const UsuarioController = {};
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+const API_URL = `${ENV.VITE_API_PROTOCOL}://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 UsuarioController.getUsuarios = async () => {
     const response = await fetch(API_URL);
